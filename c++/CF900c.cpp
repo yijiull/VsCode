27641224588e7b89d000bcfabeaf1b6c7f343f0f
yijiull <1147161372@qq.com>
2018-01-20 20:21:29 +0800

    !

--- c++/CF900c.cpp
+++ c++/CF900c.cpp
@@ -1,3 +1,6 @@
+//umax是当前最大值， lmax是当前次大值
+//分析可知，当新进入的值大于最大值时，只有移除本身使得recod减少1
+//        当新进入的值介于最大值和次大值之间时，移除最大值可使得recod增加1
 #include <bits/stdc++.h>
 using namespace std;
 const int maxn = 1e5 + 10;
